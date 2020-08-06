f4bce9279232ba3db3e2d136a996fc4e6c89cd38
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:52:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
