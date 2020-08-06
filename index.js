786ffbf43dedd990c5a2830fe109f497b0f01f94
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:50:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 const returnFirstTwoDrivers = function(drivers) {
   return drivers.slice(0,2)
 }
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(drivers.length-2, drivers.length-1)
+}
