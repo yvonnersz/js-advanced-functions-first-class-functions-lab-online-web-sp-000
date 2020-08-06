6ecbdd48c242d981500c2e13ab56d8b130dcf407
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+  let newArray = [drivers[0], drivers[1]]
+  return newArray
+}
+
+function returnLastTwoDrivers(drivers) {
+  let newArray = [drivers[drivers.length-2], drivers[drivers.length-1]]
+  return newArray
+}
+
+function selectingDrivers(drivers) {
+  let newArray = [returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)]
+  return newArray
+}
