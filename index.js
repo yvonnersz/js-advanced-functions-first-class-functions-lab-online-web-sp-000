f83fc799de2323678623f1a99b0deded5bb5e938
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const returnLastTwoDrivers = function(drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(integer) {
-  return integer*4
+let createFareMultiplier =  function(integer) {
+  return integer*4;
 }
