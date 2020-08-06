5d48bc31a56e56e7578021867f7ede1667be40f3
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function(drivers) {
-  return drivers.slice(0,2)
+  return drivers.slice(2)
 }
 
 const returnLastTwoDrivers = function(drivers) {
