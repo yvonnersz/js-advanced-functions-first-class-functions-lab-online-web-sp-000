5348ec62f9e4d4f99a98014036c78a4fe8b45ee1
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 06:46:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 // Code your solution in this file!
 
-function returnFirstTwoDrivers(drivers) {
-  let newArray = [drivers[0], drivers[1]]
-  return newArray
-}
-
-function returnLastTwoDrivers(drivers) {
-  let newArray = [drivers[drivers.length-2], drivers[drivers.length-1]]
-  return newArray
-}
-
-function selectingDrivers(drivers) {
-  let newArray = [returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)]
-  return newArray
-}
+const returnFirstTwoDrivers = function(drivers) {drivers.slice(0,2)}
